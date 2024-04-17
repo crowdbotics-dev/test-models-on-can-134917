24284621ea5864d2b598763c7a078989939f89e9
Crowdbotics <engineering@crowdbotics.com>
2024-04-17 13:00:08 +0000

    Initial commit

---
+++ backend/test_models_on_can_134917/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for test_models_on_can_134917 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "test_models_on_can_134917.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "test-models-on-can-134917",
+  url: "https://test-models-on-can-134917.botics.co",
+  api: "https://test-models-on-can-134917.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
